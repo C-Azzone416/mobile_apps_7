6b737157debc2ba24bdd0bf3f6f51942eaf6f4ed
C-Azzone416 <42585558+C-Azzone416@users.noreply.github.com>
2019-10-26 20:06:56 -0500

    contact java file started

---
+++ app/src/main/java/com/example/homework7/ContactLayout.java
@@ -0,0 +1,15 @@
+package com.example.homework7;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class ContactLayout extends MainActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_layout);
+    }
+}
--- app/src/main/java/com/example/homework7/MainActivity.java
+++ app/src/main/java/com/example/homework7/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.homework7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contact_layout);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButtonClick(View view) {
+        Intent intent = new Intent(this, ContactLayout.class);
+        startActivity(intent);
     }
 }
